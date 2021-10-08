df1b27bae438d892f81b1060a559530e5e76776f
marquimConfig <31921701+marquimConfig@users.noreply.github.com>
2021-10-08 11:13:58 -0300

    Projeto Corrigido Challenge-id-c1

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,32 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+
+    const aux = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+    setTasks(oldState => [...oldState,aux]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const isTaskCompleted = tasks.map(task => task.id == id ? {
+      ...task,
+      isComplete: !task.isComplete
+    }:task)
+
+    setTasks(isTaskCompleted);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const removeTask = tasks.filter(tasks=>tasks.id !== id);
+
+    setTasks(removeTask);
   }
 
   return (
